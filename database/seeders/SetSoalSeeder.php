ea75415a70bfaaa78f8faa706d4477cbabb43144
IMAM SODIK <imamsodik00@gmail.com>
2025-12-06 05:30:13 +0700

    asdf

--- database/seeders/SetSoalSeeder.php
+++ database/seeders/SetSoalSeeder.php
@@ -13,71 +13,170 @@ class SetSoalSeeder extends Seeder
      */
     public function run(): void
     {
+        //10
         SetSoal::create([
             'name' => 'Cambridge 10 Test 1',
+            'thumbnail' => 'Self-drive tours in the USA',
             'kode' => 'XJ3XOcvqPbgdZwyl',
         ]);
-
         SetSoal::create([
             'name' => 'Cambridge 10 Test 2',
+            'thumbnail' => 'Transport survey',
             'kode' => 'QmN0FYAE2DCXRPdC',
         ]);
-
         SetSoal::create([
             'name' => 'Cambridge 10 Test 3',
+            'thumbnail' => 'Early Learning Childcare Centre Enrolment Form',
             'kode' => 's4gzzYRpwLnhLRFf',
         ]);
-
         SetSoal::create([
             'name' => 'Cambridge 10 Test 4',
+            'thumbnail' => 'THORNDYKE’S BUILDERS',
             'kode' => '11qYaGWPJUTxUVdq',
         ]);
 
+        //14
+        SetSoal::create([
+            'name' => 'Cambridge 14 Test 1',
+            'thumbnail' => 'Crime Report Form',
+            'kode' => '1kxpl5g3zFLGtmEY',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 14 Test 2',
+            'thumbnail' => 'Total Health Clinic Patients’ Details',
+            'kode' => 'nHmZBcocwalVytdH',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 14 Test 3',
+            'thumbnail' => 'Flanders Conference Hotel',
+            'kode' => 'YuuZXlhsVNh26gHr',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 14 Test 4',
+            'thumbnail' => 'Enquiry about booking hotel for an event',
+            'kode' => '5k29sSZyLQgQjLWX',
+        ]);
+
+        //16
+        SetSoal::create([
+            'name' => 'Cambridge 16 Test 1',
+            'thumbnail' => 'Children’s Engineering Workshops',
+            'kode' => 'blsodB9LLhUn0zcg',
+        ]);
         SetSoal::create([
             'name' => 'Cambridge 16 Test 2',
+            'thumbnail' => 'Copying photos to digital format',
             'kode' => 'XW16JROLKCFE4BMH',
         ]);
         SetSoal::create([
             'name' => 'Cambridge 16 Test 3',
+            'thumbnail' => 'Junior Cycle Camp',
             'kode' => 'NmeBcwURSR2ZPfdX',
         ]);
         SetSoal::create([
             'name' => 'Cambridge 16 Test 4',
+            'thumbnail' => 'Holiday Rental',
             'kode' => 'TqMfMy8BLvZp0JDm',
         ]);
+
+        //17
         SetSoal::create([
             'name' => 'Cambridge 17 Test 1',
+            'thumbnail' => 'Buckworth Conservation Group',
             'kode' => 'QUApDKzp0Wn1GCiA',
         ]);
-
         SetSoal::create([
-            'name' => 'Cambridge 15 Test 1',
-            'kode' => 'Avfd93r1YAojm5Pb',
-        ]);
-
-        SetSoal::create([
-            'name' => 'Cambridge 15 Test 2',
-            'kode' => 'BHByU4OTwRblfc5c',
-        ]);
-
-        SetSoal::create([
-            'name' => 'Cambridge 15 Test 3',
-            'kode' => '9uAizxgIsFCinyrq',
-        ]);
-
-        SetSoal::create([
-            'name' => 'Cambridge 15 Test 4',
-            'kode' => 'GTZbRoMFOA5DZLNK',
-        ]);
-
-        SetSoal::create([
-            'name' => 'Cambridge 16 Test 1',
-            'kode' => 'blsodB9LLhUn0zcg',
-
+            'name' => 'Cambridge 17 Test 2',
+            'thumbnail' => 'Opportunities for voluntary work in Southoe village',
+            'kode' => 'DPCLyNHpDTqSciXd',
         ]);
         SetSoal::create([
-            'name' => 'Cambridge 11 Test 1',
-            'kode' => 'BoXPeTu8aF68JZFw',
+            'name' => 'Cambridge 17 Test 3',
+            'thumbnail' => 'Advice on surfing holidays',
+            'kode' => 'wq6xHskL3uTj5VSU',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 17 Test 4',
+            'thumbnail' => 'Easy Life Cleaning Services',
+            'kode' => '15gOXcE7omDCL1uE',
+        ]);
+
+        //18
+        SetSoal::create([
+            'name' => 'Cambridge 18 Test 1',
+            'thumbnail' => 'Transport survey',
+            'kode' => 'KeCD0au8jSaBuT3A',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 18 Test 2',
+            'thumbnail' => 'Working at Milo’s Restaurants',
+            'kode' => 'JSRJ0oDlxC0yFMgt',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 18 Test 3',
+            'thumbnail' => 'Wayside Camera Club',
+            'kode' => '8uQvKzYbHuROu9RJ',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 18 Test 4',
+            'thumbnail' => 'Job details from employment agency',
+            'kode' => 'z4hGzQcnxdtntccI',
+        ]);
+
+        //19
+        SetSoal::create([
+            'name' => 'Cambridge 19 Test 1',
+            'thumbnail' => 'Hinchingbrooke Country Park',
+            'kode' => 'rbsuXiTcqh8ewr9Q',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 19 Test 2',
+            'thumbnail' => 'Guitar Group',
+            'kode' => '0XIGAcSMlticROES',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 19 Test 3',
+            'thumbnail' => 'Local food shops',
+            'kode' => 'GEf4EtQ1AZxTFhTw',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 19 Test 4',
+            'thumbnail' => 'First day at work',
+            'kode' => 'IgWhHunhjhOYd3hb',
+        ]);
+
+        //20
+        SetSoal::create([
+            'name' => 'Cambridge 20 Test 1',
+            'thumbnail' => 'Restaurant Recommendations',
+            'kode' => 'xQKeqKYrkqfdgotg',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 20 Test 2',
+            'thumbnail' => 'Local Councils',
+            'kode' => 'EKX6hyJgeV3x0pxB',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 20 Test 3',
+            'thumbnail' => 'Furniture Rental Companies',
+            'kode' => 'bBhJMdwYSokWGAc9',
+        ]);
+        SetSoal::create([
+            'name' => 'Cambridge 20 Test 4',
+            'thumbnail' => 'Advice on Family Visit',
+            'kode' => '4JIjUOPpLAJ2FYdl',
+        ]);
+
+        //PT-01
+        SetSoal::create([
+            'name' => 'PT-01',
+            'thumbnail' => 'Hiring Company',
+            'kode' => '2uSKN2WwOj6EYc1X',
+        ]);
+        SetSoal::create([
+            'name' => 'PT-02',
+            'thumbnail' => 'Bedroom Furniture for Sale',
+            'kode' => 'cwwPbLf22UsNEqIp',
         ]);
     }
 }
