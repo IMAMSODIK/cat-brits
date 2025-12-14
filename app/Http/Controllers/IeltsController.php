97fd77171700da99d031bb68dd1f9ad19e9e07b9
IMAM SODIK <imamsodik00@gmail.com>
2025-12-14 22:23:50 +0700

    asdf

--- app/Http/Controllers/IeltsController.php
+++ app/Http/Controllers/IeltsController.php
@@ -134,8 +134,13 @@ class IeltsController extends Controller
                         $data['title'] = 'Cambridge IELTS 17 Academic Reading Test 1';
                         return view('ielts.categories', $data);
                         break;
+                    case 'wq6xHskL3uTj5VSU':
+                        $data['set_id'] = 'wq6xHskL3uTj5VSU';
+                        $data['title'] = 'Cambridge IELTS 17 Academic Reading Test 3';
+                        return view('ielts.categories', $data);
+                        break;
                         // 18 test 4
-                        case 'yPLnQeqINAxbrnva':
+                    case 'yPLnQeqINAxbrnva':
                         $data['set_id'] = 'yPLnQeqINAxbrnva';
                         $data['title'] = 'Cambridge IELTS 18 Academic Reading Test 4';
                         return view('ielts.categories', $data);
@@ -143,7 +148,7 @@ class IeltsController extends Controller
                         // 18 END
 
                         // 19 START
-                        case 'kNyuF8ZeAt9Trm90':
+                    case 'kNyuF8ZeAt9Trm90':
                         $data['set_id'] = 'kNyuF8ZeAt9Trm90';
                         $data['title'] = 'Cambridge IELTS 19 Academic Reading Test 1';
                         return view('ielts.categories', $data);
