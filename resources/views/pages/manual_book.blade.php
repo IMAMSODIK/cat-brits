e383a3df6680aa5d3d48b735f297543556edd0b2
IMAM SODIK <imamsodik00@gmail.com>
2026-01-08 23:44:26 +0700

    done documentation menu

--- resources/views/pages/manual_book.blade.php
+++ resources/views/pages/manual_book.blade.php
@@ -265,6 +265,8 @@
             border-radius: 10px;
         }
     </style>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
@@ -1000,6 +1002,8 @@
             });
         });
     </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
 </html>
