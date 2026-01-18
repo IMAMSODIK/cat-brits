7277a0aeba705200e87decc8a00a59a577ad8d17
IMAM SODIK <imamsodik00@gmail.com>
2026-01-19 06:29:34 +0700

    hapus auto focus pada template header dasboard

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="Typeahead Typeahead--twitterUsers">
                     <div class="u-posRelative">
                         <input class="demo-input Typeahead-input form-control-plaintext w-100" type="text"
-                            placeholder="Search Riho .." name="q" title="" autofocus>
+                            placeholder="Search Riho .." name="q" title="">
                         <div class="spinner-border Typeahead-spinner" role="status"><span class="sr-only">Loading...
                             </span></div><i class="close-search" data-feather="x"></i>
                     </div>
